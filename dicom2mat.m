1cc35f26dc504046697e47663a6545462a8eff0a
awil220 <66495166+awil220@users.noreply.github.com>
2022-06-01 22:59:43 -0700

    Update dicom2mat.m

--- dicom2mat.m
+++ dicom2mat.m
@@ -14,9 +14,6 @@ for i=1:num
     if contains(list(i).name,'.dcm')
         x = dicomread(list(i).name);
         save([list(i).name(1:size(list(i).name,2)-3),'mat'],'x');
-    elseif contains(list(i).name,'.dicom')
-        x = dicomread(list(i).name);
-        save([list(i).name(1:size(list(i).name,2)-5),'mat'],'x');
     end
 end
 
